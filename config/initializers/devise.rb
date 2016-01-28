1422dba680ca816ed7633cbffd0174a90668de84
Dan Solo <dan@solocreative.com.au>
2016-01-28 18:01:22 +1100

    Last if statement for secure variables back to front

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -29,7 +29,7 @@ Devise.setup do |config|
   config.omniauth :facebook, API_KEYS['FACEBOOK_APP_ID'], API_KEYS['FACEBOOK_SECRET_APP_ID']
   require 'omniauth-google-oauth2'
   OpenSSL::SSL::VERIFY_PEER = OpenSSL::SSL::VERIFY_NONE if Rails.env.development?
-  if Rails.env.production?
+  if Rails.env.development?
     config.omniauth :google_oauth2, API_KEYS['GOOGLE_APP_ID'], API_KEYS['GOOGLE_SECRET_APP_ID'], { access_type: "offline", approval_prompt: "", scope: "email" }
   else
     config.omniauth :google_oauth2, ENV['GOOGLE_APP_ID'], ENV['GOOGLE_SECRET_APP_ID'], { access_type: "offline", approval_prompt: "", scope: "email" }
