8646b991317b41186f6d6a3c835c5049bdbaa2de
Dan Solo <dan@solocreative.com.au>
2016-01-30 09:47:50 +1100

    Forgot to add config for Facebook to prod in devise.rb

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -30,9 +30,10 @@ Devise.setup do |config|
   OpenSSL::SSL::VERIFY_PEER = OpenSSL::SSL::VERIFY_NONE if Rails.env.development?
   if Rails.env.development?
     config.omniauth :google_oauth2, API_KEYS['GOOGLE_APP_ID'], API_KEYS['GOOGLE_SECRET_APP_ID'], { access_type: "offline", approval_prompt: "", scope: "email" }
-    config.omniauth :facebook, API_KEYS['FACEBOOK_APP_ID'], API_KEYS['FACEBOOK_SECRET_APP_ID'], scope: 'email', info_fields: 'email, name'
+    config.omniauth :facebook, API_KEYS['FACEBOOK_APP_ID'], API_KEYS['FACEBOOK_SECRET_APP_ID'], {scope: 'email', info_fields: 'email, name'}
   else
     config.omniauth :google_oauth2, ENV['GOOGLE_APP_ID'], ENV['GOOGLE_SECRET_APP_ID'], { access_type: "offline", approval_prompt: "", scope: "email" }
+    config.omniauth :facebook, ENV['FACEBOOK_APP_ID'], ENV['FACEBOOK_SECRET_APP_ID'], {scope: 'email', info_fields: 'email, name'}
   end
   # Configure the class responsible to send e-mails.
   # config.mailer = 'Devise::Mailer'
